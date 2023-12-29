95caabea5b1f87796f51893f80d1fb66fb53c565
Igor Kosteski <igor@groovygecko.com>
2023-12-29 14:18:48 +0100

    EE-358 show stage only for producer

--- components/Room/index.tsx
+++ components/Room/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { DailyAudio } from '@daily-co/daily-react';
 
 import { Modals } from '@/components/Room/Modals';
@@ -7,15 +7,24 @@ import { Stage } from '@/components/Room/Stage';
 import { Tray } from '@/components/Room/Tray';
 import { LayoutSwitchMenu } from '@/components/Room/Tray/LayoutSwitchMenu';
 import { VcsPreview } from '@/components/Room/Vcs';
+import { usePathname } from 'next/navigation';
 
 export function Room() {
+
+  const pathname = usePathname();
+  const [roleAdmin, setRoleAdmin] = useState<boolean>(false);
+
+  useEffect(() => {
+    setRoleAdmin(pathname.split('/').pop() === 'producer');
+  }, [pathname, setRoleAdmin])
+
   return (
     <div className="flex-1">
       <div className="flex h-full">
         <div className="relative flex w-full flex-1 flex-col md:w-[calc(100%-400px)]">
           <VcsPreview />
           <LayoutSwitchMenu />
-          <Stage />
+          { roleAdmin ? <Stage /> : ''}
           <Tray />
         </div>
         <Sidebar />
